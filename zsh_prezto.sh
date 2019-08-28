d292d5e579db7024a4b10eb24252e5ea3f50446f
Ngữ Quang Trương <zquangu112z@users.noreply.github.com>
2019-08-29 00:56:52 +0700

    Update zsh_prezto.sh

--- zsh_prezto.sh
+++ zsh_prezto.sh
@@ -7,4 +7,20 @@ setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
+# auto start zprezto
 echo "source ${ZDOTDIR:-$HOME}/.zprezto/init.zsh" >> ~/.zshrc
+
+# suggest module: open .zpreztorc and add these module:
+# zstyle ':prezto:load' pmodule \
+#   'environment' \
+#   'terminal' \
+#   'editor' \
+#   'history' \
+#   'history-substring-search' \
+#   'directory' \
+#   'spectrum' \
+#   'utility' \
+#   'completion' \
+#   'prompt' \
+#   'git' \
+#   'autosuggestions'
